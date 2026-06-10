511e1fa6d277c4e433a51fa236cf7c5c7e63a9ce
bilaapy943@gmail.com <bilaapy943@gmail.com>
2026-06-10 22:47:57 +0700

    mengubah desain tombol

--- app/page.tsx
+++ app/page.tsx
@@ -180,7 +180,7 @@ useEffect(() => {
 
 <div className="flex justify-center gap-10 mt-4">
   <Link href="/">
-    <button className="w-200 px-5 py-2 rounded-full bg-blue-900 text-white hover:bg-yellow-400 transition">
+    <button className=" px-5 py-2 rounded-full bg-blue-900 text-white hover:bg-yellow-600 transition">
      Home
   </button>
   </Link>
