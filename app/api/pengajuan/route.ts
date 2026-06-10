c90d0dec066e474e20b913453366981d391fa14d
rikiardiansyah-web <riki_ardiansyah@teknokrat.ac.id>
2026-06-11 00:44:11 +0700

    nambah api pengajuan

---
+++ app/api/pengajuan/route.ts
@@ -0,0 +1,51 @@
+import { prisma } from "@/src/lib/prisma";
+
+export async function GET() {
+  try {
+
+    const pengajuan = await prisma.Pengajuan.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    return Response.json(pengajuan);
+
+  } catch (error) {
+
+    console.error(error);
+
+    return Response.json(
+      { error: "Gagal mengambil pengajuan" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(req: Request) {
+  try {
+
+    const body = await req.json();
+
+    const pengajuan =
+      await prisma.Pengajuan.create({
+        data: {
+          nik: body.nik,
+          nama: body.nama,
+          judul: body.judul,
+          deskripsi: body.deskripsi,
+        },
+      });
+
+    return Response.json(pengajuan);
+
+  } catch (error) {
+
+    console.error(error);
+
+    return Response.json(
+      { error: "Gagal membuat pengajuan" },
+      { status: 500 }
+    );
+  }
+}
